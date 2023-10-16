1f45a206b1e67a61c905859e3eb4e1fc6b555922
Oliver Theabould <olivertheabould@Olivers-MacBook-Pro-2.local>
2023-10-16 17:29:49 +0100

    add better tests for createUser schema validation

--- __tests__/handlers/createUser/handler.int.test.ts
+++ __tests__/handlers/createUser/handler.int.test.ts
@@ -8,26 +8,19 @@ const baseUrl = process.env.apiBaseURL;
 
 describe("createUser handler", () => {
   it("should respond with statusCode 200 to correct request", async () => {
-    // GIVEN
-    const email = await getFakeEmail();
-
     const payload: SchemaBody = {
-      email: email,
+      email: await getFakeEmail(),
       password: "fakepassword11@@",
       name: "fake name",
       avatarS3Key: null,
     };
-    // WHEN
     const actual = await axios.post<UserResponse>(`${baseUrl}`, payload);
 
-    // THEN
     expect(actual.status).toBe(200);
   });
 
   it("should respond with created user", async () => {
-    // GIVEN
     const email = await getFakeEmail();
-
     const payload: SchemaBody = {
       email: email,
       password: "fakepassword11@@",
@@ -35,22 +28,23 @@ describe("createUser handler", () => {
       avatarS3Key: null,
     };
 
-    // WHEN
     const response = await axios.post<UserResponse>(`${baseUrl}`, payload);
     const actual = response.data;
 
-    // THEN
     expect(actual.id).toBeTruthy();
     expect(actual.email).toBe(email);
     expect(actual.name).toBe("fake name");
     expect(actual.avatarS3Key).toBe(null);
   });
 
-  it("should respond with Bad Request 400 to incorrect request", async () => {
-    // GIVEN
-    const wrongPayload = {};
+  it("should respond with Bad Request 400 with an invalid email", async () => {
+    const wrongPayload = {
+      email: "invalid email",
+      password: "fakepassword11@@",
+      name: "fake name",
+      avatarS3Key: null,
+    };
 
-    // WHEN
     let actual;
     try {
       await axios.post(`${baseUrl}`, wrongPayload);
@@ -58,13 +52,47 @@ describe("createUser handler", () => {
       actual = e.response;
     }
 
-    // THEN
+    expect(actual.status).toBe(400);
+    expect(actual.statusText).toBe("Bad Request");
+  });
+
+  it("should respond with Bad Request 400 with an empty name", async () => {
+    const wrongPayload = {
+      email: await getFakeEmail(),
+      password: "fakepassword11@@",
+      avatarS3Key: null,
+    };
+
+    let actual;
+    try {
+      await axios.post(`${baseUrl}`, wrongPayload);
+    } catch (e) {
+      actual = e.response;
+    }
+
+    expect(actual.status).toBe(400);
+    expect(actual.statusText).toBe("Bad Request");
+  });
+
+  it("should respond with Bad Request 400 with an empty password", async () => {
+    const wrongPayload = {
+      email: await getFakeEmail(),
+      name: "Obi-Wan",
+      avatarS3Key: null,
+    };
+
+    let actual;
+    try {
+      await axios.post(`${baseUrl}`, wrongPayload);
+    } catch (e) {
+      actual = e.response;
+    }
+
     expect(actual.status).toBe(400);
     expect(actual.statusText).toBe("Bad Request");
   });
 
   it("should respond with Conflict 409 for an existing email", async () => {
-    // GIVEN
     const existingUser = await createTestUser();
 
     const payload: SchemaBody = {
@@ -74,16 +102,16 @@ describe("createUser handler", () => {
       avatarS3Key: null,
     };
 
-    // WHEN
     let actual;
     try {
       await axios.post(`${baseUrl}`, payload);
     } catch (e) {
       actual = e.response;
     }
-
-    // THEN
     expect(actual.status).toBe(409);
     expect(actual.statusText).toBe("Conflict");
+    expect(actual.data.message).toBe(
+      "A user with the provided email already exists.",
+    );
   });
 });
