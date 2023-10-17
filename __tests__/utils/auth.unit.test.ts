631430cd38c3006e1a8660403df58b3b643129b5
Oliver Theabould <olivertheabould@Olivers-MacBook-Pro-2.local>
2023-10-17 18:07:23 +0100

    add `authenticatePassword` helper

--- __tests__/utils/auth.unit.test.ts
+++ __tests__/utils/auth.unit.test.ts
@@ -1,4 +1,4 @@
-import { hashPassword } from "@utils/auth";
+import { authenticatePassword, hashPassword } from "@utils/auth";
 
 describe("auth", () => {
   describe("hashPassword", () => {
@@ -16,6 +16,26 @@ describe("auth", () => {
       const hash2 = hashPassword(password);
 
       expect(hash1).not.toEqual(hash2);
+    });
+  });
+
+  describe("authenticatePassword", () => {
+    it("should return true for a matching password", async () => {
+      const password = "fake password";
+      const hash = hashPassword(password);
+
+      const isValid = await authenticatePassword(password, hash);
+      expect(isValid).toBe(true);
+    });
+
+    it("should return false for a matching password", async () => {
+      const password = "fake password";
+      const hash = hashPassword(password);
+
+      const wrongPassword = "something else";
+
+      const isValid = await authenticatePassword(wrongPassword, hash);
+      expect(isValid).toBe(false);
     });
   });
 });
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,3 +4,11 @@ export const hashPassword = (plainPassword: string) => {
   const hash = bcrypt.hashSync(plainPassword, 9);
   return hash;
 };
+
+export const authenticatePassword = async (
+  plainPassword: string,
+  hash: string,
+) => {
+  const isValid = await bcrypt.compare(plainPassword, hash);
+  return isValid;
+};
